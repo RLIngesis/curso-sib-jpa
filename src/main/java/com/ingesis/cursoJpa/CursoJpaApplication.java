e295e903d0564894b62509e4c2b52eedbee1fd9c
rony.lemus <rony.lemus@ingesis.com>
2022-02-11 15:10:00 -0600

    Se quitaron comentarios de código.

--- src/main/java/com/ingesis/cursoJpa/CursoJpaApplication.java
+++ src/main/java/com/ingesis/cursoJpa/CursoJpaApplication.java
@@ -3,8 +3,6 @@ package com.ingesis.cursoJpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableAutoConfiguration
-//@ComponentScan(basePackages="com.ingesis")
 @SpringBootApplication
 public class CursoJpaApplication {
 
