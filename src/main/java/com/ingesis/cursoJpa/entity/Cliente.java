7331257782f24dcb2662492af5a3a465cb7be23e
Javier Castillo <javier.castillo@ingesis.com>
2022-02-14 17:49:47 -0600

    Metodo equals y hascode para cliente

--- src/main/java/com/ingesis/cursoJpa/entity/Cliente.java
+++ src/main/java/com/ingesis/cursoJpa/entity/Cliente.java
@@ -1,6 +1,7 @@
 package com.ingesis.cursoJpa.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -38,53 +39,23 @@ public class Cliente implements Serializable {
     @Column(nullable = false)
     private String email;
 
-	public Integer getIdCliente() {
-		return idCliente;
+	@Override
+	public int hashCode() {
+		return Objects.hash(idCliente);
 	}
 
-	public void setIdCliente(Integer idCliente) {
-		this.idCliente = idCliente;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(idCliente, other.idCliente);
 	}
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-
-	public String getNit() {
-		return nit;
-	}
-
-	public void setNit(String nit) {
-		this.nit = nit;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
     
      
 }
